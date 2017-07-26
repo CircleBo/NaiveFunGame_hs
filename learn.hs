3475996b513c7707e6114e91751310aa70bb95de
YPBlib <ypb1024@foxmail.com>
2017-07-26 08:45:49 +0800

    6

---
+++ learn.hs
@@ -0,0 +1,97 @@
+--BOOL
+--True || False
+--True && False
+--not True
+--odd 7
+--2 == 3
+--2 /= 3
+-- ^:integer **:real
+--(mod 10 3 )== 1
+--min 8 9
+--max 5 6
+--succ 8
+--(div (-5) (-2))==2
+--(div (-5) (2))==-3
+doubleMe x = x+x
+doublesmall x = if (>) x 2
+    then x
+    else (*) x 2
+-- <Only in GHCi
+--let a = 1>
+---[1,2,3]++[34,5]
+-- 2 : [3,4,5,6]
+--[0,1,2,3,4] !! 2 == 2    ####Index
+--[3,2,1] > [2,1,0]    ####lexicographical order
+--[3,4,2] > [3,4]
+--head last init tail
+--length [1,2,3]
+-- null reverse
+--maximum minimum
+--drop take sum product
+--elem 'a' ['a'..'z']
+--generating infinite lists: cycle for list, repeat for element
+--finite: replicate
+--replicate 3 10
+--list comprehension
+--[x+2|x<-[1..100],mod x 2 ==1]
+--
+--
+--
+----
+--
+--
+--
+--
+--
+--
+--
+---
+--
+--
+--
+--
+--
+--
+--
+----
+--
+--
+--
+--
+--
+--
+--
+----
+--
+--
+--
+--
+--
+--
+--
+--
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
