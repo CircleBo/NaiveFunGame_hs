59b5233e66cf470348f0fa9b5b147d8bf25f3e5a
YPBlib <ypb1024@foxmail.com>
2017-10-12 18:12:44 +0800

    m

--- typeclass.hs
+++ typeclass.hs
@@ -4,6 +4,11 @@ class Functor f where
 (<$>) :: Functor f => (a -> b) -> f a -> f b
 infixl 4 <$>
 
+(<$) :: Functor f => a -> f b -> f a
+infixl 4 <$
+-- The default definition is fmap . const, 
+--but this may be overridden with a more efficient version.
+
 --------------------------------
 --fmap id == id
 --fmap (f.g) == fmap f . fmap g
@@ -11,15 +16,32 @@ infixl 4 <$>
 
 instance Functor [] where  
     fmap = map  
+
+instance Functor Maybe where  
+    fmap f (Just x) = Just (f x)  
+    fmap f Nothing = Nothing
+
+-- A phantom type is a parametrised type whose parameters
+--do not all appear on the right-hand side of its definition.
+newtype Const a b = Const { getConst :: a }
+
+instance Functor (Const a)where
+    fmap f c = c
+-- Verify   
+    --fmap id == id
+    --fmap (f.g) == fmap f . fmap g
+
 -------------------------------------------------------------------------------------------
 class (Functor f) => Applicative f where  
     pure :: a -> f a  
     (<*>) :: f (a -> b) -> f a -> f b
-
+(<*>) :: Applicative f => f (a -> b) -> f a -> f b
+infixl 4 <*>
 liftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
 --A minimal complete definition must include implementations of pure and of either <*> or liftA2. 
 --If it defines both, then they must behave the same as their default definitions:
---(<*>) == liftA2 id liftA2 f x y == f <$> x <*> y
+--(<*>) == liftA2 id 
+--liftA2 f x y == f <$> x <*> y
 
 --------------------------------------------
 --identity
@@ -32,23 +54,40 @@ liftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
 --u <*> pure y == pure ($y) <*> u
 --------------------------------------------
 
-
-
-(<*>) :: Applicative f => f (a -> b) -> f a -> f b
-infixl 4 <*>
-
 (<**>) :: Applicative f => f a -> f (a -> b) -> f b
 infixl 4 <**>
-    
+
+--Sequence actions, discarding the value of the first argument:
+(*>) :: Applicative f => f a -> f b -> f b
+infixl 4 *>
+
+--Sequence actions, discarding the value of the second argument:
 (<*) :: Applicative f => f a -> f b -> f a
 infixl 4 <*
 
-(*>) :: Applicative f => f a -> f b -> f b
-infixl 4 <*
-instance Applicative Maybe where  
-    pure = Just  
-    Nothing <*> _ = Nothing  
-    (Just f) <*> something = fmap f something
+--default definitions, which may be overridden with equivalent specialized implementations:
+u *> v = (id <$ u) <*> v
+u <* v = liftA2 const u v
+
+--------------------------------------------
+instance Applicative [] where  
+    pure x = [x]  
+    fs <*> xs = [f x | f <- fs, x <- xs] 
+    liftA2 f xs ys = [f x y | x <- xs, y <- ys]
+    xs *> ys  = [y | _ <- xs, y <- ys]
+--------------------------------------------
+instance Applicative Maybe where
+    pure = Just
+
+    Just f  <*> m       = fmap f m
+    Nothing <*> _m      = Nothing
+
+    liftA2 f (Just x) (Just y) = Just (f x y)
+    liftA2 _ _ _ = Nothing
+
+    Just _m1 *> m2      = m2
+    Nothing  *> _m2     = Nothing
+
 -------------------------------------------------------------------------------------------
 class Monad m where  
     return :: a -> m a  
@@ -60,12 +99,54 @@ class Monad m where
     
     fail :: String -> m a  
     fail msg = error msg  
+-------------------------------------------------------------------------------------------
+--------------------------------------------
+class Applicative m => Monad m where
+    -- | Sequentially compose two actions, passing any value produced
+    -- by the first as an argument to the second.
+    (>>=)       :: forall a b. m a -> (a -> m b) -> m b
+
+    -- | Sequentially compose two actions, discarding any value produced
+    -- by the first, like sequencing operators (such as the semicolon)
+    -- in imperative languages.
+    (>>)        :: forall a b. m a -> m b -> m b
+    m >> k = m >>= \_ -> k -- See Note [Recursive bindings for Applicative/Monad]
+    {-# INLINE (>>) #-}
+
+    -- | Inject a value into the monadic type.
+    return      :: a -> m a
+    return      = pure
+
+    -- | Fail with a message.  This operation is not part of the
+    -- mathematical definition of a monad, but is invoked on pattern-match
+    -- failure in a @do@ expression.
+    --
+    -- As part of the MonadFail proposal (MFP), this function is moved
+    -- to its own class 'MonadFail' (see "Control.Monad.Fail" for more
+    -- details). The definition here will be removed in a future
+    -- release.
+    fail        :: String -> m a
+    fail s      = errorWithoutStackTrace s
+
+--------------------------------------------
+join              :: (Monad m) => m (m a) -> m a
+join x            =  x >>= id
+
+ap                :: (Monad m) => m (a -> b) -> m a -> m b
+ap m1 m2          = do { x1 <- m1; x2 <- m2; return (x1 x2) }
+-- Since many Applicative instances define (<*>) = ap, we
+-- cannot define ap = (<*>)
 
 (<$!>) :: Monad m => (a -> b) -> m a -> m b
 infixl 4 <$!>
 
 (>>=) :: Monad m => m a -> (a -> m b) -> m b
 infixl 1 >>=
+
+--Same as '>>=', but with the arguments interchanged    
+(=<<) :: Monad m => (a -> m b) -> m a -> m b
+infixr 1 =<<
+f =<< x         = x >>= f
 
 (>>) :: Monad m => m a -> m b -> m b
 infixl 1 >>
--- typeclass.md
+++ typeclass.md
@@ -1,4 +1,9 @@
-1. infixl 即 取左边的参数为第一个参数？
-    a `infixl` b == infixl a b
-    a `infixr` b == infixr b a
-2.
+
+1. 把"class Functor f where  
+    fmap :: (a -> b) -> f a -> f b "
+   "class (Functor f) => Applicative f where  
+    pure :: a -> f a  
+    (<*>) :: f (a -> b) -> f a -> f b"成为Functor，Applicative的定义？
+2. "u *> v = (id <$ u) <*> v" , 当 u==Nothing？？？
+    u *> v = v 难道不是更省事？
+3.  
