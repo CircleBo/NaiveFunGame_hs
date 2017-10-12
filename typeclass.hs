a0a592f92ec85fb1ab8aa92cef5597cbc6f35533
YPBlib <ypb1024@foxmail.com>
2017-10-12 15:56:26 +0800

    m

--- typeclass.hs
+++ typeclass.hs
@@ -1,18 +1,55 @@
 class Functor f where  
     fmap :: (a -> b) -> f a -> f b 
 
+(<$>) :: Functor f => (a -> b) -> f a -> f b
+infixl 4 <$>
+
+--------------------------------
+--fmap id == id
+--fmap (f.g) == fmap f . fmap g
+--------------------------------
+
 instance Functor [] where  
     fmap = map  
-
+-------------------------------------------------------------------------------------------
 class (Functor f) => Applicative f where  
     pure :: a -> f a  
     (<*>) :: f (a -> b) -> f a -> f b
 
+liftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
+--A minimal complete definition must include implementations of pure and of either <*> or liftA2. 
+--If it defines both, then they must behave the same as their default definitions:
+--(<*>) == liftA2 id liftA2 f x y == f <$> x <*> y
+
+--------------------------------------------
+--identity
+--pure id <*> v == v    
+--composition
+--pure (.) <*> u <*> v <*> == u <*> (v <*>)
+--homomorphism
+--pure f <*> pure x = pure (f x)
+--interchange
+--u <*> pure y == pure ($y) <*> u
+--------------------------------------------
+
+
+
+(<*>) :: Applicative f => f (a -> b) -> f a -> f b
+infixl 4 <*>
+
+(<**>) :: Applicative f => f a -> f (a -> b) -> f b
+infixl 4 <**>
+    
+(<*) :: Applicative f => f a -> f b -> f a
+infixl 4 <*
+
+(*>) :: Applicative f => f a -> f b -> f b
+infixl 4 <*
 instance Applicative Maybe where  
     pure = Just  
     Nothing <*> _ = Nothing  
     (Just f) <*> something = fmap f something
-
+-------------------------------------------------------------------------------------------
 class Monad m where  
     return :: a -> m a  
     
@@ -24,9 +61,21 @@ class Monad m where
     fail :: String -> m a  
     fail msg = error msg  
 
+(<$!>) :: Monad m => (a -> b) -> m a -> m b
+infixl 4 <$!>
+
+(>>=) :: Monad m => m a -> (a -> m b) -> m b
+infixl 1 >>=
+
+(>>) :: Monad m => m a -> m b -> m b
+infixl 1 >>
+---------------------------------------------
+--return a >>= k  =  k a
+--m >>= return  =  m
+--m >>= (\x -> k x >>= h)  =  (m >>= k) >>= h
+---------------------------------------------
 instance Monad Maybe where  
     return x = Just x  
     Nothing >>= f = Nothing  
     Just x >>= f  = f x  
     fail _ = Nothing
-
---
+++ typeclass.md
@@ -0,0 +1,4 @@
+1. infixl 即 取左边的参数为第一个参数？
+    a `infixl` b == infixl a b
+    a `infixr` b == infixr b a
+2.
